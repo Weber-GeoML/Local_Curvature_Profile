94900086415e59b018e1e439492d50b3c6a91622
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2024-12-08 10:30:03 -0500

    Update hyperparams.py

--- hyperparams.py
+++ hyperparams.py
@@ -22,7 +22,11 @@ def get_args_from_input():
 	parser.add_argument('--hidden_dim', metavar='', type=int, help='width of hidden layer')
 	parser.add_argument('--hidden_layers', metavar='', type=ast.literal_eval, help='list containing dimensions of all hidden layers')
 	parser.add_argument('--num_layers', metavar='', type=int, help='number of hidden layers')
-	parser.add_argument('--num_splits', metavar='', type=int, default=3, help='Number of random splits')
+	
+	# Comment: in an earlier version of this repository, the default for num_splits was set to 3, not 1 
+	# as described in our paper. We have corrected this and found no statistically significant difference 
+	# for any of the node classification experiments.
+	parser.add_argument('--num_splits', metavar='', type=int, default=1, help='Number of random splits')
 	parser.add_argument('--batch_size', metavar='', type=int, help='number of samples in each training batch')
 	parser.add_argument('--num_trials', metavar='', type=int, help='number of times the network is trained'),
 	parser.add_argument('--rewiring', metavar='', type=str, help='type of rewiring to be performed'),
